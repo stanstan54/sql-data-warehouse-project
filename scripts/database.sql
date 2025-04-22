9395f26b3d2c30e7aa4d6b5ac4424185ef2b005a
stanstan54 <97313834+stanstan54@users.noreply.github.com>
2025-04-22 14:10:02 -0400

    Update database.sql

--- scripts/database.sql
+++ scripts/database.sql
@@ -1,1 +1,27 @@
+===============================================================
+  This was created for database backup.
+================================================================
+IF EXISTS (SELECT 1 FROM system.databases WHERE name = 'Datawarehouse')
+  BEGIN
+    ALTER DATABASE Datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE Datawarehouse;
+END;
+--Set GO to initate--
+GO
+  
+  /*create datebase */
+  
+CREATE DATABASE Datawarehouse;
+GO
+
+USE Datawarehouse;
+GO
+
+--create the schemas--
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver; 
+GO
+CREATE SCHEMA gold;
+GO
 
