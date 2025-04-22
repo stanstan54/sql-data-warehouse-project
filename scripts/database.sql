f4dbce52b973110d6d069ad992c5b017f5cab359
stanstan54 <97313834+stanstan54@users.noreply.github.com>
2025-04-22 14:33:31 -0400

    Update database.sql

--- scripts/database.sql
+++ scripts/database.sql
@@ -1,6 +1,13 @@
+/*
 ===============================================================
   This was created for database backup.
 ================================================================
+Purpose:
+  This script is used to create a database called 'Datawarehouse'. If this already exisit, the database will drop. Also, this
+  will create three schemas called 'bronze', 'silver', and 'gold'. 
+
+CAUTION ⚠: 
+  Be sure you have the proper back up when running this script. If not, this will 
 IF EXISTS (SELECT 1 FROM system.databases WHERE name = 'Datawarehouse')
   BEGIN
     ALTER DATABASE Datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
